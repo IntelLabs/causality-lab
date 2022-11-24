95f000233db49041bbf123b0a69fbefb904aee36
Raanan Rohekar <raanan.y.yehezkel.rohekar@intel.com>
2022-11-24 21:07:32 +0200

    bug fix: edge orientation in homology

--- causal_discovery_algs/ts_icd.py
+++ causal_discovery_algs/ts_icd.py
@@ -1,13 +1,11 @@
-import warnings
-
 from causal_discovery_algs.icd import LearnStructICD, unique_element_iterator
-from graphical_models import PAG, PDSTree, arrow_head_types as Mark
+from graphical_models import PAG, arrow_head_types as Mark
 from itertools import product, combinations, chain
 
 
 class LearnStructTSICD(LearnStructICD):
     def __init__(self, nodes_sets_list, ci_test, is_homology=True, initial_pag: PAG = None, is_pre_calc_cond_set=False,
-                 is_selection_bias=True, is_tail_completeness=True):
+                 is_selection_bias=False, is_tail_completeness=True):
         """
         Initialization: create a complete PAG with o--> edges between past-future and o--o between contemporaneous nodes
         :param nodes_sets_list: list of list of nodes.
@@ -22,12 +20,11 @@ class LearnStructTSICD(LearnStructICD):
         :param is_selection_bias: is selection bias possibly present? (default: True)
         :param is_tail_completeness: should the algorithm apply rules R8, R9, R10 from Zhang 2008b (default: True)
         """
-        warnings.warn("TS-ICD is currently under development, bugs may be present", RuntimeWarning)
         win_len = len(nodes_sets_list)  # time steps: t=0, t=-1, ..., t=-(win_len-1)
         assert win_len > 1  # at least two time-steps are expected
 
         num_nodes_t0 = len(nodes_sets_list[0])  # number of nodes in a single time-step
-        assert any((isinstance(nodes_t, list) and len(nodes_t) == num_nodes_t0)
+        assert all((isinstance(nodes_t, list) and len(nodes_t) == num_nodes_t0)
                    for nodes_t in nodes_sets_list)  # check equal length of sub-lists, and that they are list types
 
         nodes_set = {node for nodes_t_i in nodes_sets_list for node in nodes_t_i}  # unwrap all the nodes into one set
@@ -111,34 +108,6 @@ class LearnStructTSICD(LearnStructICD):
             homology.append((node_0_new, node_1_new))
         return homology
 
-    def get_ci_homology_1(self, node_0, node_1, conditioning_tuple) -> list:
-        """
-        A homology map finds all the node-pairs that are homologous to a given pair.
-        Homology of conditioning sets is restricted to the time window length. That is, ignoring conditioning sets
-        that are outside the time-window, but still returning the node-pair as independent.
-        :param node_0: node at present time
-        :param node_1: node in present time (contemporaneous) or in the past (cross-lag)
-        :param conditioning_tuple: conditioning set in present time or in the past
-        :return: a list of tuples, where each tuple corresponds to one homologous triplet (node, node, conditioning set)
-        """
-        assert self.node2time[node_0] == 0  # node_0 must be from the current time-stamp
-        time_1 = self.node2time[node_1]
-        time_conditioning = [self.node2time[conditioning_node] for conditioning_node in conditioning_tuple]
-        node_1_idx = self.node2idx[node_1]
-        node_0_idx = self.node2idx[node_0]  # index of the node in the present (t=0)
-        conditioning_idx = [self.node2idx[conditioning_node] for conditioning_node in conditioning_tuple]
-        past_time = time_1  # conditioning sets might be further back in time. Those outside the time-window: omitted
-        homology = []
-        # loop through new time indexes for node_0  and node_i (iteratively move one step back in time)
-        for idx, _ in enumerate(range(past_time+1, self.time_window_len)):
-            node_0_new = self._get_node_name(1+idx, node_0_idx)
-            node_1_new = self._get_node_name(time_1+1+idx, node_1_idx)
-            conditioning_tuple_new = tuple(self._get_node_name(node_time+1+idx, node_idx)
-                                           for node_time, node_idx in zip(time_conditioning, conditioning_idx)
-                                           if node_time+1+idx < self.time_window_len)  # only withing the time-window
-            homology.append((node_0_new, node_1_new, conditioning_tuple_new))
-        return homology
-
     def get_ci_homology(self, node_0, node_1, conditioning_tuple) -> list:
         """
         A homology map that finds all the node-pairs & conditioning sets that are homologous.
@@ -187,30 +156,18 @@ class LearnStructTSICD(LearnStructICD):
         """
         if cond_set_size is None:
             assert self.is_pre_calc_pds is True
-        cond_indep = self.ci_test.cond_indep
-        source_pag = self.graph  # Not a copy!!! thus, edge deletions affect consequent CI queries
 
-        done1 = self._learn_struct_incremental_for_edges(cond_set_size, self.possible_cross_lag_edges)  # reduce exogenous
-        done2 = self._learn_struct_incremental_for_edges(cond_set_size, self.possible_contemporaneous_edges)  # contemporaneous
+        # reduce exogenous
+        done1 = self._learn_struct_incremental_for_edges(cond_set_size, self.possible_cross_lag_edges)
+        # reduce contemporaneous
+        done2 = self._learn_struct_incremental_for_edges(cond_set_size, self.possible_contemporaneous_edges)
         done = done1 and done2
 
         # Orient edges
-        # ------------
-        if not done:  # re-orient the skeleton only if it was modified
-            self.graph.reset_orientations(default_mark=Mark.Circle)
-            self.set_fixed_orientations()
-            self.graph.orient_v_structures(self.sepset)  # corresponds to rule R0
-            self.graph.maximally_orient_pattern(rules_set=[1, 2, 3, 4])
-        else:  # algorithm concluded, orient all edges for obtaining completeness
-            if self.is_selection_bias:
-                self.graph.maximally_orient_pattern(rules_set=[5, 6, 7])  # when selection-bias may be present
-            if self.is_tail_completeness:
-                self.graph.maximally_orient_pattern(rules_set=[8, 9, 10])  # for tail-completeness
-
+        self.orient_graph(complete_orientation=done)
         return done
 
     def _learn_struct_incremental_for_edges(self, cond_set_size, possible_edges):
-        # cond_indep = self.ci_test.cond_indep
         done = True
         for node_i, node_j in possible_edges:
             if not self.graph.is_connected(node_i, node_j):
@@ -226,14 +183,6 @@ class LearnStructTSICD(LearnStructICD):
                 cond_set = cond[0]  # get the set of nodes (in [1] there is the sum-of-minimal-distances)
                 cond_tup = tuple(cond_set)
                 is_edge_removed = self._test_edge_and_remove_in_homology(node_i, node_j, cond_tup)
-                # if cond_indep(node_i, node_j, cond_tup):
-                #     self.graph.delete_edge(node_i, node_j)  # remove directed/undirected edge
-                #     self.sepset.set_sepset(node_i, node_j, cond_tup)
-                #     if self.is_homology:
-                #         homology = self.get_homology(node_0=node_i, node_1=node_j)
-                #         for (node_0, node_1) in homology:
-                #             self.graph.delete_edge(node_0, node_1)
-                #             self.sepset.set_sepset(node_0, node_1, cond_tup)
                 if is_edge_removed:
                     break  # stop searching for independence as we found one and updated the graph accordingly
         return done
@@ -271,8 +220,7 @@ class LearnStructTSICD(LearnStructICD):
         for node_i, node_j in self.possible_contemporaneous_edges:
             self._test_edge_and_remove_in_homology(node_i, node_j, ())
 
-        self.graph.orient_v_structures(self.sepset)
-        self.graph.maximally_orient_pattern(rules_set=[1, 2, 3, 4])
+        self.orient_graph(complete_orientation=False)
 
     def _learn_struct_base_step_1(self):
         """
@@ -281,7 +229,6 @@ class LearnStructTSICD(LearnStructICD):
 
         :return: True if done and no more iterations are required; otherwise False indicating the PAG is not completed.
         """
-        cond_indep = self.ci_test.cond_indep
         source_cpdag = self.graph  # Not a copy!!! Thus, edge deletions affect consequent CI queries
 
         # r = 1: conditional independence tests order 1
@@ -305,9 +252,6 @@ class LearnStructTSICD(LearnStructICD):
             for cond_set in cond_sets:
                 done = False
                 is_edge_removed = self._test_edge_and_remove_in_homology(node_i, node_j, cond_set)
-                # if cond_indep(node_i, node_j, cond_set):
-                #     self.graph.delete_edge(node_i, node_j)  # remove directed/undirected edge
-                #     self.sepset.set_sepset(node_i, node_j, cond_set)
                 if is_edge_removed:
                     break  # stop searching for independence as we found one and updated the graph accordingly
 
@@ -328,19 +272,60 @@ class LearnStructTSICD(LearnStructICD):
             for cond_set in cond_sets:
                 done = False
                 is_edge_removed = self._test_edge_and_remove_in_homology(node_i, node_j, cond_set)
-                # if cond_indep(node_i, node_j, cond_set):
-                #     self.graph.delete_edge(node_i, node_j)  # remove directed/undirected edge
-                #     self.sepset.set_sepset(node_i, node_j, cond_set)
                 if is_edge_removed:
                     break  # stop searching for independence as we found one and updated the graph accordingly
 
+        self.orient_graph(complete_orientation=done)
+        return done
+
+    def orient_graph(self, complete_orientation=True):
         self.graph.reset_orientations(default_mark=Mark.Circle)
         self.set_fixed_orientations()
         self.graph.orient_v_structures(self.sepset)
+        self._fill_orientation_homology()
         self.graph.maximally_orient_pattern(rules_set=[1, 2, 3, 4])
-        if self.is_selection_bias:
-            self.graph.maximally_orient_pattern(rules_set=[5, 6, 7])  # when selection-bias may be present
-        if self.is_tail_completeness:
-            self.graph.maximally_orient_pattern(rules_set=[8, 9, 10])  # for tail-completeness
+        if complete_orientation:
+            if self.is_selection_bias:
+                self.graph.maximally_orient_pattern(rules_set=[5, 6, 7])  # when selection-bias may be present
+            if self.is_tail_completeness:
+                self.graph.maximally_orient_pattern(rules_set=[8, 9, 10])  # for tail-completeness
+        self._fill_orientation_homology()
 
-        return done
+    def _fill_orientation_homology(self):
+        """
+        Two consecutive stages:
+            1. Create a list of invariant edge-marks (head or tail)
+            2. For each edge-mark set edge-marks throughout the homology
+        :return:
+        """
+        # Step 1: Create a list of invariant edge-marks (head or tail)
+        invariant_marks = []  # list of invariant edge marks in the graph
+        for node1, node2 in combinations(self.graph.nodes_set, 2):
+            if not self.graph.is_connected(node1, node2):
+                continue
+            edge_mark12 = self.graph.get_edge_mark(node1, node2)
+            edge_mark21 = self.graph.get_edge_mark(node2, node1)
+            if edge_mark12 != Mark.Circle:
+                invariant_marks.append((node1, node2, edge_mark12))
+            if edge_mark21 != Mark.Circle:
+                invariant_marks.append((node2, node1, edge_mark21))
+
+        # Step 2: For each edge-mark set edge-marks throughout the homology
+        for parent, child, edge_mark in invariant_marks:
+            parent_time = self.node2time[parent]
+            child_time = self.node2time[child]
+            min_time = min(parent_time, child_time)
+            parent_base = self.nodes_sets_list[parent_time - min_time][self.node2idx[parent]]
+            child_base = self.nodes_sets_list[child_time - min_time][self.node2idx[child]]
+            if self.node2time[child_base] == 0:
+                edge_homology = self.get_edge_homology(child_base, parent_base)
+            elif self.node2time[parent_base] == 0:
+                edge_homology = self.get_edge_homology(parent_base, child_base)
+                edge_homology = [(b, a) for (a, b) in edge_homology]  # reverse a,b
+            else:
+                raise RuntimeError("unexpected error")
+            edge_homology.append((child_base, parent_base))
+
+            for (child_node, parent_node) in edge_homology:
+                self.graph.replace_edge_mark(node_source=parent_node, node_target=child_node,
+                                             requested_edge_mark=edge_mark)
